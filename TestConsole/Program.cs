d33e4ca1784b6d11c20ae280a5f4bb12b3212ad0
Joe Cashmore <joseph.cashmore@pertemps.co.uk>
2023-03-22 10:32:08 +0000

    inital

---
+++ TestConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿using PNGSchemaGenerator.SchemaTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            var jobPosting = new JobPosting()
+            {
+                Title = "developer",
+                AlternateName = "shit",
+                BaseSalary  = 27
+            };
+
+            Console.WriteLine(jobPosting.ToString());
+            Console.ReadKey();
+        }
+    }
+}
